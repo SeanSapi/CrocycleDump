9d03b4359b752348e9016be1fe351ed3f107ceee
Sean Sapi <sapitanansean@gmail.com>
2023-04-09 20:01:19 +0400

    patch 0.3
    "We decided to leave the main page on pause, and create the login page."
    
    - Created the login board - (40%)
    
    What is next?
    - Finishing it.

---
+++ Crocycle/Web files/LoginPage.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <div class="black">
+
+
+        </div>
+
+
+        <div class="login">
+
+           <label><b>User Name
+           </b>
+           </label>
+           <input type="text" name="Uname" id="Uname" placeholder="Username">
+           <br><br>
+           <label><b>Password
+           </b>
+           </label>
+           <input type="Password" name="Pass" id="Pass" placeholder="Password">
+           <br><br>
+           <input type="button" name="log" id="log" value="Log In Here">
+           <br><br>
+           <input type="checkbox" id="check">
+           <span>Remember me</span>
+           <br><br>
+           Forgot <a href="#">Password</a>
+
+        </div>
+    </div>
+
+</body>
+</html>
